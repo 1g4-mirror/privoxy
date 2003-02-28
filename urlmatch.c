8e8cad911acb4f51b44668e3f5b14b037ccec1e6
oes <oes@users.sourceforge.net>
2003-02-28 13:09:29 +0000

    Fixed a rare double free condition as per Bug #694713

--- urlmatch.c
+++ urlmatch.c
@@ -1,4 +1,4 @@
-const char urlmatch_rcs[] = "$Id: urlmatch.c,v 1.10.2.3 2002/11/12 16:50:40 oes Exp $";
+const char urlmatch_rcs[] = "$Id: urlmatch.c,v 1.10.2.4 2003/02/28 12:57:44 oes Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/Attic/urlmatch.c,v $
@@ -33,6 +33,10 @@ const char urlmatch_rcs[] = "$Id: urlmatch.c,v 1.10.2.3 2002/11/12 16:50:40 oes 
  *
  * Revisions   :
  *    $Log: urlmatch.c,v $
+ *    Revision 1.10.2.4  2003/02/28 12:57:44  oes
+ *    Moved freeing of http request structure to its owner
+ *    as per Dan Price's observations in Bug #694713
+ *
  *    Revision 1.10.2.3  2002/11/12 16:50:40  oes
  *    Fixed memory leak in parse_http_request() reported by Oliver Stoeneberg. Fixes bug #637073
  *
@@ -257,12 +261,11 @@ jb_err parse_http_url(const char * url,
          http->hostport = strdup(url_noproto);
       }
 
-      free(buf);
+      freez(buf);
 
       if ( (http->path == NULL)
         || (http->hostport == NULL))
       {
-         free(buf);
          return JB_ERR_MEMORY;
       }
    }
